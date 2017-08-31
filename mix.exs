a5370ae35834653bde475332dcadd0dce6ecbc45
Andrew Vy <andrew@andrewvy.com>
2017-08-30 22:10:33 -0700

    :bookmark: v0.0.1
    OSX-only version

--- mix.exs
+++ mix.exs
@@ -4,10 +4,14 @@ defmodule ChromeLauncher.Mixfile do
   def project do
     [
       app: :chrome_launcher,
-      version: "0.1.0",
+      version: "0.0.1",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
-      deps: deps()
+      deps: deps(),
+      name: "Chrome Launcher",
+      source_url: "https://github.com/andrewvy/chrome-launcher",
+      description: description(),
+      package: package()
     ]
   end
 
@@ -21,7 +25,22 @@ defmodule ChromeLauncher.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:erlexec, "~> 1.7"}
+      {:erlexec, "~> 1.7"},
+      {:ex_doc, ">= 0.0.0", only: :dev}
+    ]
+  end
+
+  defp description do
+    "Utility library for launching managed chrome processes"
+  end
+
+  defp package do
+    [
+      maintainers: ["andrew@andrewvy.com"],
+      licenses: ["MIT"],
+      links: %{
+        "Github" => "https://github.com/andrewvy/chrome-launcher"
+      }
     ]
   end
 end
