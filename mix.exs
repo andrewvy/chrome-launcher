b7e72add0ac706380a71a32acc7a00a5ebf955de
Andrew Vy <andrew@andrewvy.com>
2017-10-26 09:42:35 -0700

    :bookmark: 0.0.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ChromeLauncher.Mixfile do
   def project do
     [
       app: :chrome_launcher,
-      version: "0.0.2",
+      version: "0.0.3",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
       deps: deps(),
